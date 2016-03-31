0b5262d35c92a86042bcbb11039da8970c0d84b9
lkiversonlk <city.of.beijing@gmail.com>
2016-04-01 00:25:10 +0800

    fix a little bug

--- public/js/index.js
+++ public/js/index.js
@@ -8,22 +8,24 @@ var ReactDom = require("react-dom");
 var ImgViewPanel = require("./components/imgViewPanel");
 var ImagePicker = require("./components/viewImagePicker");
 
-var picker = new ImagePicker();
+//var picker = new ImagePicker();
 
 ReactDom.render(
     <div>
         <div className="row">
             <div className="col-md-6 col-xs-6" style={{padding : 0}}>
-                <ImgViewPanel imagePicker={picker}></ImgViewPanel>
+                <ImgViewPanel imagePicker={new ImagePicker()}></ImgViewPanel>
             </div>
             <div className="col-md-6 col-xs-6" style={{padding : 0}}>
-                <ImgViewPanel imagePicker={picker}></ImgViewPanel>
+                <ImgViewPanel imagePicker={new ImagePicker()}></ImgViewPanel>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-md-6 col-xs-6" style={{padding : 0}}>
+                <ImgViewPanel imagePicker={new ImagePicker()}></ImgViewPanel>
             </div>
             <div className="col-md-6 col-xs-6" style={{padding : 0}}>
-                <ImgViewPanel imagePicker={picker}></ImgViewPanel>
-            </div>
-            <div className="col-md-6 col-xs-6" style={{padding : 0}}>
-                <ImgViewPanel imagePicker={picker}></ImgViewPanel>
+                <ImgViewPanel imagePicker={new ImagePicker()}></ImgViewPanel>
             </div>
         </div>
     </div>
