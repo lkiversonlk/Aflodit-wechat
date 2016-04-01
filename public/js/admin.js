2c42adcd61acf71025843f7d31f5ca3660ac6ef9
lkiversonlk <city.of.beijing@gmail.com>
2016-04-01 22:41:30 +0800

    change the panel button.
    add Readme

--- public/js/admin.js
+++ public/js/admin.js
@@ -20,56 +20,14 @@ reactDOM.render(
                 图片审核
             </p>
         </a>
-        <a href="/admin/audit" className="weui_grid js_grid" data-id="audit">
+        <a href="/admin/message" className="weui_grid js_grid" data-id="audit">
             <div className="weui_grid_icon">
-                <img src="/images/icon_nav_toast.png"/>
+                <img src="/images/icon_nav_dialog.png"/>
             </div>
             <p className="weui_grid_label">
-                图片审核
+                联系我们
             </p>
         </a>
-
-        <a href="/admin/audit" className="weui_grid js_grid" data-id="audit">
-            <div className="weui_grid_icon">
-                <img src="/images/icon_nav_toast.png"/>
-            </div>
-            <p className="weui_grid_label">
-                图片审核
-            </p>
-        </a>
-        <a href="/admin/audit" className="weui_grid js_grid" data-id="audit">
-            <div className="weui_grid_icon">
-                <img src="/images/icon_nav_toast.png"/>
-            </div>
-            <p className="weui_grid_label">
-                图片审核
-            </p>
-        </a>
-        <a href="/admin/audit" className="weui_grid js_grid" data-id="audit">
-            <div className="weui_grid_icon">
-                <img src="/images/icon_nav_toast.png"/>
-            </div>
-            <p className="weui_grid_label">
-                图片审核
-            </p>
-        </a>
-        <a href="/admin/audit" className="weui_grid js_grid" data-id="audit">
-            <div className="weui_grid_icon">
-                <img src="/images/icon_nav_toast.png"/>
-            </div>
-            <p className="weui_grid_label">
-                图片审核
-            </p>
-        </a>
-        <a href="/admin/audit" className="weui_grid js_grid" data-id="audit">
-            <div className="weui_grid_icon">
-                <img src="/images/icon_nav_toast.png"/>
-            </div>
-            <p className="weui_grid_label">
-                图片审核
-            </p>
-        </a>
-
     </div>,
     document.getElementById("main")
 );
